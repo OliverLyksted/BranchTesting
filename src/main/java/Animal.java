91ab930a9ac5fd45b2e40ad00b0ee7316865da37
OliverLyksted <144331612+OliverLyksted@users.noreply.github.com>
2023-11-13 13:15:58 +0100

    commit 1.5

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -3,4 +3,6 @@ public abstract class Animal {
     private String name;
 
     public abstract void makeSound();
+
+    public abstract boolean eat(String Food);
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,10 @@ public class Main {
     public static void main(String[] args) {
         Dog dog = new Dog();
         dog.makeSound();
+        dog.eat("kødben");
         Cat cat = new Cat();
         cat.makeSound();
+        cat.eat("fisk");
 
     }
 }
